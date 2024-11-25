8a5acc6c52d0e7d383521dd58b98ab014e1265ec
nouscode <juancspace@outlook.com>
2024-11-24 21:36:41 -0500

    build(gaos): crate and set up spring project
    using docker file for running spring project

---
+++ gaos/src/main/java/com/gaos/gaos/GaosApplication.java
@@ -0,0 +1,13 @@
+package com.gaos.gaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaosApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+}
